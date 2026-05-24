39bff498f8a58fdd3b62b5920f8222df36147f0e
Yousef Obeida <yousef_4764@limu.edu.ly>
2026-05-24 14:56:18 +0200

    fix: replaced welcome.blade to json response

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json(['message' => 'Smart HR API']);
 });
 
 Route::get('/test', function () {
